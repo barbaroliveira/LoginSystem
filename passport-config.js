d493730a3f66903f5505baa74df3eeb7e4a0a0d1
Bárbara Pereira Poças Oliveira <89589498+barbaroliveira@users.noreply.github.com>
2023-11-27 23:21:34 +0000

    login funtionality needs fix

--- passport-config.js
+++ passport-config.js
@@ -18,7 +18,8 @@ function initialize(passport, getUserByEmail) {
         }
     }
 
-    passport.use(new LocalStrategy({ usernameField: 'email'}), authenticateUser)
+    passport.use(new LocalStrategy({ usernameField: 'email'},
+     authenticateUser))
     passport.serializeUser((user, done) => {  })
     passport.deserializeUser((id, done) => {  })
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV !== 'production') {
-    require('dorenv').config()
+    require('dotenv').config()
 }
 
 const express = require('express')
