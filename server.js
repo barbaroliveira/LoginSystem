361bb99927b1ac04993bb4e55be8709455c4c7c9
Bárbara Pereira Poças Oliveira <89589498+barbaroliveira@users.noreply.github.com>
2023-11-27 15:35:15 +0000

    post for register form

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+const { redirect } = require('react-router-dom')
+
+const users = []
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {name: 'Kyle'})
@@ -11,12 +16,29 @@ app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
 
+app.post('/login', (req, res) => {
+    //finish this code when the login form is complete
+})
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) => {
-    //finish this code when the form is complete
+app.post('/register', async(req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            //in case of an database this date is going to be
+            //automatically atribueted
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
 })
 
 app.listen(3000)
